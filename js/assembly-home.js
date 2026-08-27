c93a3c9afda140a40828def2eed80190500fc12b
Evan Prodromou <evan@prodromou.name>
2026-08-27 11:51:29 -0400

    feat: load groups

--- js/assembly-home.js
+++ js/assembly-home.js
@@ -48,6 +48,10 @@ export class AssemblyHomeElement extends AssemblyElement {
       min-width: 14rem;
     }
 
+    .join-group {
+      white-space: nowrap;
+    }
+
     @media (max-width: 640px) {
       header {
         align-items: stretch;
@@ -74,30 +78,17 @@ export class AssemblyHomeElement extends AssemblyElement {
       _error: { type: String, state: true },
       _actor: { type: Object, state: true },
       _groups: { type: Array, state: true },
+      _groupsLoading: { type: Boolean, state: true },
       _groupActorId: { type: String, state: true }
     }
   }
 
   constructor () {
     super()
-    this._groups = [
-      {
-        actor: {
-          id: 'https://example.com/groups/film',
-          type: 'Group',
-          name: 'Film Club'
-        }
-      },
-      {
-        actor: {
-          id: 'https://example.com/groups/books',
-          type: 'Group',
-          name: 'Book Group'
-        }
-      }
-    ]
-    this._groupActorId = this._groups[0].actor.id
-    this._route = this._groupRoute(this._groupActorId)
+    this._groups = []
+    this._groupsLoading = false
+    this._groupActorId = null
+    this._route = ''
   }
 
   connectedCallback () {
@@ -106,6 +97,7 @@ export class AssemblyHomeElement extends AssemblyElement {
     this.getActor()
       .then((actor) => {
         this._actor = actor
+        return this._loadGroups(actor)
       })
       .catch((err) => {
         this._error = err.message
@@ -119,17 +111,20 @@ export class AssemblyHomeElement extends AssemblyElement {
     <header>
 
       <div class="primary-nav">
-        <span class="brand"><a href=${`#${this._groupRoute(this._groupActorId)}`}>Assembly</a></span>
+        <span class="brand"><a href=${this._groupActorId ? `#${this._groupRoute(this._groupActorId)}` : '#'}>Assembly</a></span>
 
-        <sl-select
-          class="group-select"
-          value=${this._groupActorId}
-          @sl-change=${this._groupChange.bind(this)}
-        >
-          ${this._groups.map(group => html`
-            <sl-option value=${group.actor.id}>${group.actor.name}</sl-option>
-          `)}
-        </sl-select>
+        ${this._groups.length > 0
+          ? html`<sl-select
+              class="group-select"
+              placeholder="Groups"
+              value=${this._groupActorId}
+              @sl-change=${this._groupChange.bind(this)}
+            >
+              ${this._groups.map(group => html`
+                <sl-option value=${group.actor.id}>${group.actor.name}</sl-option>
+              `)}
+            </sl-select>`
+          : html`<sl-button class="join-group" disabled>Join Group</sl-button>`}
       </div>
 
       <!-- User menu dropdown -->
@@ -155,7 +150,7 @@ export class AssemblyHomeElement extends AssemblyElement {
     </header>
 
     <main>
-      ${this._route.startsWith('group/')
+      ${this._route.startsWith('group/') && this._groupActorId
         ? html`<assembly-group
             redirect-uri=${this.redirectUri}
             client-id=${this.clientId}
@@ -163,6 +158,13 @@ export class AssemblyHomeElement extends AssemblyElement {
           </assembly-group>`
         : (this._route === 'assembly')
           ? html`<assembly-new-note redirect-uri=${this.redirectUri}   client-id=${this.clientId} />`
+          : this._groupsLoading
+            ? html`<sl-spinner></sl-spinner>`
+            : this._groups.length === 0
+              ? html`
+                  <p>No groups yet.</p>
+                  <sl-button disabled>Join Group</sl-button>
+                `
           : html`<sl-alert>Unknown route</sl-alert>`
       }
     </main>
@@ -192,10 +194,49 @@ export class AssemblyHomeElement extends AssemblyElement {
       }
   }
 
+  async _loadGroups (actor) {
+    this._groupsLoading = true
+    try {
+      const collection = actor.groups ?? actor.following
+      if (!collection) {
+        this._groups = []
+        return
+      }
+
+      const groups = []
+      for await (const item of this.items(collection)) {
+        const group = await this.toObject(item, { required: ['id', 'type'] })
+        if (this._isGroupActor(group) && !groups.some(existing => existing.actor.id === group.id)) {
+          groups.push({ actor: group })
+          this._groups = [...groups]
+        }
+      }
+
+      if (!this._groupActorId && this._groups.length > 0) {
+        this._groupActorId = this._groups[0].actor.id
+        this._route = this._groupRoute(this._groupActorId)
+        window.history.replaceState(null, '', `#${this._route}`)
+      }
+    } finally {
+      this._groupsLoading = false
+    }
+  }
+
+  _isGroupActor (actor) {
+    if (!actor?.type) {
+      return false
+    }
+    return Array.isArray(actor.type)
+      ? actor.type.includes('Group')
+      : actor.type === 'Group'
+  }
+
   _updateRoute () {
     const route = (window.location.hash)
       ? window.location.hash.replace('#', '')
-      : this._groupRoute(this._groupActorId)
+      : this._groupActorId
+        ? this._groupRoute(this._groupActorId)
+        : ''
     if (route === 'logout') {
       this._logout()
     } else {
@@ -210,6 +251,9 @@ export class AssemblyHomeElement extends AssemblyElement {
   }
 
   _groupRoute (actorId) {
+    if (!actorId) {
+      return ''
+    }
     return `group/${actorId.replace(/^https:\/\//, '')}`
   }
 
